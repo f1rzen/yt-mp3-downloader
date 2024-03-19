7a53a1b37f122aa0a528087886516e37569705b0
Hasan Talha Çelik <35665454+bitterkofte@users.noreply.github.com>
2024-03-19 13:46:53 +0300

    Select Comp Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { Button, Flex, Text } from "@radix-ui/themes";
+import { Button, Flex, Select, Text } from "@radix-ui/themes";
+import { useState } from "react";
+import { downloadFileTypes } from "./utils/data";
 
 function App() {
+  const [selectedResolution, setSelectedResolution] = useState<string>(downloadFileTypes[0].value);
   return (
 <>
     <Flex direction="column" gap="2" justify={"center"} align={"center"}>
       <Text>Hello from Radix Themes :)</Text>
       <Button>Let's go</Button>
+      <Select.Root defaultValue={downloadFileTypes[0].value}>
+        <Select.Trigger />
+        <Select.Content position="popper">
+          {downloadFileTypes.map((e,i) => (
+            <Select.Item onClick={() => setSelectedResolution(e.value)} key={i} value={e.value}>{ e.label }</Select.Item>
+          ))}
+        </Select.Content>
+      </Select.Root>
+      <Text>{ selectedResolution }</Text>
     </Flex>
 </>
   );
---
+++ src/utils/data.ts
@@ -0,0 +1,22 @@
+export const downloadFileTypes = [
+  {
+    value: "mp4-1080p",
+    label: "Video (.mp4 1080p)",
+  },
+  {
+    value: "mp4-720p",
+    label: "Video (.mp4 720p)",
+  },
+  {
+    value: "mp4-480p",
+    label: "Video (.mp4 480p)",
+  },
+  {
+    value: "mp3-320kbps",
+    label: "Audio (.mp3 320Kbps)",
+  },
+  {
+    value: "mp3-166kbps",
+    label: "Audio (.mp3 320Kbps)",
+  },
+]
