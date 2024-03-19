338f1afe2eb484feacc51b3e493ed598b479a660
İlkay <ilkaybaytar@aol.com>
2024-03-19 13:42:04 +0300

    tailwind install

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button, Flex, Text } from "@radix-ui/themes";
 function App() {
   return (
 <>
-    <Flex direction="column" gap="2" justify={"center"} align={"center"}>
+    <Flex direction="column" gap="2" justify={"center"} align={"center"} className="h-screen flex items-center justify-center">
       <Text>Hello from Radix Themes :)</Text>
       <Button>Let's go</Button>
     </Flex>
